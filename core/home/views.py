0225bc8db0cdebe117b89cbcde9f266fd50e272f
Shubham Tripathi <shubhhtr@gmail.com>
2023-09-12 01:18:44 +0530

    Python code in HTML

--- core/home/views.py
+++ core/home/views.py
@@ -7,4 +7,11 @@ def home(request):
     return HttpResponse("<h2>Testing</h2>")
 
 def contacts(request):
-    return render(request, "contact.html")
+
+    data = [
+        {'name':'Test', 'age':23, 'gender': 'male'},
+        {'name':'qwerty', 'age':33, 'gender': 'male'},
+        {'name':'pooja', 'age':20, 'gender': 'female'},
+    ]
+
+    return render(request, "contact.html", context= {'data': data})
