b805ff87d16cc7e615e8a7b864862b96114fd172
Shubham Tripathi <shubhhtr@gmail.com>
2023-09-13 08:52:51 +0530

    Completed adding recipe functionality

--- core/recipe/views.py
+++ core/recipe/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
 
 def recipes(request):
-    data = request.POST
-    print(data)
+    if(request.method == 'POST'):
+        data = request.POST
+        recipe_image = request.FILES.get('recipe_image')
+        recipe_name = data.get('recipe_name')
+        recipe_description = data.get('recipe_description')
+
+        Recipes.objects.create(recipe_name = recipe_name, recipe_description = recipe_description, recipe_image = recipe_image)
+        return redirect('/recipes/')
+
     return render(request, 'recipes.html')
